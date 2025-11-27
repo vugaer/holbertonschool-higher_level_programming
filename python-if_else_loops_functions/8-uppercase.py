8caf8ffbdcf2e4832ca5f36b4125593da406b749
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-11-27 05:57:14 +0400

    asdasdasd

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -9,3 +9,16 @@ def islower(word):
         return False
     elif 97 <= ord(word) <= 122:
         return True
+    
+def make_upper(word):
+    return chr(ord(word)-32)
+    
+def uppercase(anything):
+    for i in anything:
+        if islower(i):
+            print(make_upper(i), end='')
+        else:
+            print(i, end='')
+
+
+uppercase('best school IN the world')
