cd46d7702981755c8cc5be146392882dbf8e2626
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-11-27 06:06:40 +0400

    asdasdasdasd

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,22 +1,9 @@
 #!/usr/bin/python3
 
-
-def islower(ch):
-    if 65 <= ord(ch) <= 90:
-        return False
-    elif 97 <= ord(ch) <= 122:
-        return True
-    else:
-        return False
-
-
-def make_upper(ch):
-    if islower(ch):
-        return chr(ord(ch) - 32)
-    else:
-        return ch
-
-
-def uppercase(text):
-    for ch in text:
-        print("{}".format(make_upper(ch)), end='')
+def uppercase(str):
+    for ch in str:
+        code = ord(ch)
+        if 97 <= code <= 122:        # 'a'..'z'
+            code = code - 32         # convert to 'A'..'Z'
+        print("{:c}".format(code), end='')
+    print()
