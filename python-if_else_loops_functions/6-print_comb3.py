d54284feb1fbb281e549eccf4d40c3ea7a3184ce
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-11-27 05:23:32 +0400

    asdasdad

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-for i in range(1, 10):
-    for j in range(1, 10):
+for i in range(0, 10):
+    for j in range(0, 10):
         if i < j:
             if i == 8 and j == 9:
                 print(89)
             else:
                 print('{0}{1}'.format(str(i),str(j)), end=', ')
+            
