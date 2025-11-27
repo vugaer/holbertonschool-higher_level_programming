ba973036b81e77c083735232ca1d23ace02fa156
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-11-27 04:51:40 +0400

    qwasdasdasd

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for alphabet in range(ord('a'), ord('z')+1):
-    if chr(alphabet) != "e" or chr(alphabet) != "q":
-        print("{}".format(chr(alphabet)), end='')
-    else:
+    if chr(alphabet) == "q" or chr(alphabet) == "e":
         pass
+    else:
+        print(chr(alphabet), end='')
