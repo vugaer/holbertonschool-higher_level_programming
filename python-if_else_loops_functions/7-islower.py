966b9bdb51f3eef2ff097cfe11b3cf3b6def1acd
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-11-27 05:31:33 +0400

    asdasd

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# print(ord('a'))
+# print(ord('z'))
+# print(ord('A'))
+# print(ord('Z'))
+
+def islower(word):
+    if 65 <= ord(word) <= 90:
+        return False
+    elif 97 <= word(word) <= 122:
+        return True
