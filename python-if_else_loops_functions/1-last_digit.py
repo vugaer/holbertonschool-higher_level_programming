05502eb84b7c302b4edd74e87d68c35906608bd4
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-11-27 04:35:58 +0400

    gitpusher

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+
+number = random.randint(-10000, 10000)
+
+last = int(str(number)[-1])
+
+if last > 5:
+    condition = "greater than 5"
+elif last < 5:
+    condition = "less than 6 and not 0"
+elif last == 0:
+    condition = "0"
+
+print(f"Last digit of {number} is {last} and is {condition}")
