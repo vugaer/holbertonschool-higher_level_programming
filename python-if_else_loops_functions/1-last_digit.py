b3fd764892f184f319d903b4b552bb563f827555
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-11-27 05:37:22 +0400

    asdasdasd

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import random
-
+sign = ' '
 number = random.randint(-10000, 10000)
 
 last = int(str(number)[-1])
@@ -8,10 +8,10 @@ last = int(str(number)[-1])
 if last < 5 or number < 0:
     condition = "less than 6 and not 0"
     if number < 0:
-        sign = "-"
+        sign = " -"
 elif last > 5 and number > 0:
     condition = "greater than 5"
 elif last == 0:
     condition = "0"
 
-print(f"Last digit of {number} is {sign}{last} and is {condition}")
+print(f"Last digit of {number} is{sign}{last} and is {condition}")
