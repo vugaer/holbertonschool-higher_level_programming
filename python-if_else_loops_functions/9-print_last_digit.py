0a3b5db74430a3fcb174ce7ec1c4f3116971f9a1
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-11-27 06:18:05 +0400

    asdasdasd

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-yurr = ''
-
 def print_last_digit(number):
-    global yurr
-    if number > 0:
-        last = number % 10
-    else:
+    if number < 0:
         last = -number % 10
-    
-    yurr = yurr + str(last)
-
-print(yurr)
+    else:
+        last = number % 10
+    print(last, end="")
+    return last
