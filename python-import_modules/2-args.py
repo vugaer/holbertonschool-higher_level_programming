ed1eb52b2fbaec7d48682bb7d48b8d8a7e969d54
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-12-03 19:19:22 +0400

    gitpusher

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,6 +4,8 @@ if __name__ == "__main__":
     argv = sys.argv
     if len(argv) == 1:
         print('0 arguments.')
+    elif len(argv) == 2:
+        print('1 argument:\n{}: {}'.format(1, argv[1]))
     else:
         print('{} arguments:\n'.format(len(argv)- 1))
         for i in range(1, len(argv)):
