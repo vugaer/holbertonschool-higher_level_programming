aad11200dd8873f9683cb6d896f75625d38d27f5
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-12-03 19:16:01 +0400

    gitpusher

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import sys
-argv = sys.argv
-if len(argv) == 1:
-    print('0 arguments.')
-else:
-    print('{} arguments:\n'.format(len(argv)- 1))
-    for i in range(1, len(argv)):
-        print('{}: {}'.format(i, argv[i]))
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    if len(argv) == 1:
+        print('0 arguments.')
+    else:
+        print('{} arguments:\n'.format(len(argv)- 1))
+        for i in range(1, len(argv)):
+            print('{}: {}'.format(i, argv[i]))
