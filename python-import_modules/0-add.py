9d72cb8e2b3013c4ab7e187a2540d395e77fe73d
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-12-03 18:07:45 +0400

    gitpusher

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import add_0 as wtf
+    import add_0 as meagre
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, wtf(a, b)))
+    print("{} + {} = {}".format(a, b, meagre.add(a, b)))
