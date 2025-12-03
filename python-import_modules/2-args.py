67da69f30da088f8093b38956330ebc6df48e16a
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-12-03 19:25:26 +0400

    gitpusher

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,7 +7,6 @@ if __name__ == "__main__":
     elif len(argv) == 2:
         print('1 argument:\n{}: {}'.format(1, argv[1]))
     else:
-        print('{} arguments:'.format(len(argv)- 1))
+        print('{} arguments:'.format(len(argv) - 1))
         for i in range(1, len(argv)):
             print('{}: {}'.format(i, argv[i]))
-
