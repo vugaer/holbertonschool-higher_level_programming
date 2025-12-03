a36851decf10834c3aff491941cd606826bb9d8c
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-12-03 17:57:36 +0400

    gitpusher

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def add(a, b):
-	print("{} + {} = {}".format(a, b, a + b))
-if __name__ == "__main__":
-	add(1, 2)
+import add_0
+a = 1
+b = 2
+print("{} + {} = {}".format(a, b, add_0(a, b)))
