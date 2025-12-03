0bd29f4f7e41f70ecbba8f2d0ca614cf8af89a72
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-12-03 18:16:53 +0400

    gitpusher

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -2,7 +2,8 @@
 import calculator_1 as meagre
 a = 10
 b = 5
-print("{} + {} = {}".format(a, b, meagre.add(a, b)))
-print("{} - {} = {}".format(a, b, meagre.sub(a, b)))
-print("{} * {} = {}").format(a, b, meagre.mul(a, b))
-print("{} / {} = {}".format(a, b, meagre.div(a, b)))
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, meagre.add(a, b)))
+    print("{} - {} = {}".format(a, b, meagre.sub(a, b)))
+    print("{} * {} = {}".format(a, b, meagre.mul(a, b)))
+    print("{} / {} = {}".format(a, b, meagre.div(a, b)))
