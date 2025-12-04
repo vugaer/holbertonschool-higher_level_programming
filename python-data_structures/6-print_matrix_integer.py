343b08f6038f0dbbeeb9af0fdb23d29759f0c8ac
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-12-04 18:27:39 +0400

    gitpusher

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for item in row:
-            print("{:d}".format(item), end=' ')
-        print('\n')
+            print("{:2d}".format(item), end='')
+        print('{:d}'.format('\n'))
