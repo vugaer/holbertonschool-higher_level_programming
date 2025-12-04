3178d065c6d14034255cb8cf92dd3e5cf8965e84
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-12-04 19:31:13 +0400

    gitpusher

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix == [[]]:
-        return ''
-    else:
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if j < (len(matrix[0]) - 1):
-                    print("{:d}".format(matrix[i][j]), end=' ')
-                elif j == (len(matrix[0]) - 1):
-                    print("{:d}".format(matrix[i][j]), end='')
-            print()
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if j < (len(matrix[0]) - 1):
+                print("{:d}".format(matrix[i][j]), end=' ')
+            elif j == (len(matrix[0]) - 1):
+                print("{:d}".format(matrix[i][j]), end='')
+        print()
 
 # oh shit
