8941d238c3477c82a62b1d47b3a7fed79a86aef0
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-12-07 23:12:50 +0400

    gitpusher

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,8 +3,9 @@
 def best_score(a_dictionary):
     holder_best = None
     if a_dictionary == {}:
-        return None
-    for i in a_dictionary:
-        if a_dictionary[i] == max(a_dictionary.values()):
-            holder_best = i
-    return holder_best
+        pass
+    else:
+        for i in a_dictionary:
+            if a_dictionary[i] == max(a_dictionary.values()):
+                holder_best = i
+        return holder_best
