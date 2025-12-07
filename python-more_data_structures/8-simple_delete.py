2a378b56a9f0eb089d1b9d04cf66f6ba86452e8c
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-12-07 22:35:35 +0400

    gitpusher

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
-    del a_dictionary[key]
-    return a_dictionary
+    empty_dict = {}
+    for i in a_dictionary:
+        if i != key:
+            empty_dict[i] = a_dictionary[i]
+    return empty_dict
