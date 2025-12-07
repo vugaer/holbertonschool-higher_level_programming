78dce8f1ad467772e6cc2cc5afd08c7189e5329e
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-12-07 16:30:28 +0400

    gitpusher

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    epstein = []
+    for i in range(2):
+        epstein += [tuple_a[i] + tuple_b[i]]
+    return tuple(epstein)
+
+def unleash_potential(standart):
+    if len(standart) < 2:
+        
+
+# tuple_a = (1, )
+# tuple_b = (88, 11)
+# new_tuple = add_tuple(tuple_a, tuple_b)
+# print(new_tuple)
+# print(add_tuple(tuple_a, (1, )))
+# print(add_tuple(tuple_a, ()))
