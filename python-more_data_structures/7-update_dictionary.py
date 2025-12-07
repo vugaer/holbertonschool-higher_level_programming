b67307263a9df802517827fccd0ffded784276d8
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-12-07 22:31:16 +0400

    gitpusher

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -3,9 +3,3 @@
 def update_dictionary(b_dictionary, key, value):
     b_dictionary[key] = value
     return b_dictionary
-
-# dict = {"a": "A"}
-# print(dict)
-
-# update_dictionary(dict, "a", "A")
-# print(dict)
