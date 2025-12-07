5aead9a2ca91f7bdbad8626059d20c943a088403
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-12-07 23:47:51 +0400

    gitpusher

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,7 @@
-#!/usr/bin/python3
-
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    
+
     roman_map = {
         'I': 1,
         'V': 5,
@@ -13,17 +11,20 @@ def roman_to_int(roman_string):
         'D': 500,
         'M': 1000
     }
-    
+
     result = 0
     i = 0
-    
+
     while i < len(roman_string):
-        # If current value is less than next value, subtract it
-        if i + 1 < len(roman_string) and roman_map.get(roman_string[i], 0) < roman_map.get(roman_string[i + 1], 0):
-            result += roman_map[roman_string[i + 1]] - roman_map[roman_string[i]]
-            i += 2  # Skip next character since we already processed it
+        current_val = roman_map.get(roman_string[i], 0)
+        next_val = roman_map.get(roman_string[i + 1], 0) if i + 1 < len(
+            roman_string) else 0
+
+        if current_val < next_val:
+            result += next_val - current_val
+            i += 2
         else:
-            result += roman_map.get(roman_string[i], 0)
+            result += current_val
             i += 1
-    
+
     return result
