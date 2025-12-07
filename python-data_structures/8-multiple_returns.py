7747620cbd94de83111d00ef15993c07a81ef39c
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-12-07 17:31:05 +0400

    gitpusher

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    return (len(sentence), sentence[0])
+    if sentence == "":
+        return (0, None)
+    else:
+        return (len(sentence), sentence[0])
