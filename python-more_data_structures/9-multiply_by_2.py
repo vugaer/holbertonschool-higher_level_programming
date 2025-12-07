8d15cc5d176eea5aeafbf2abdaf4ef4a95b33b16
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-12-07 22:54:14 +0400

    gitpusher

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-def multiply_by_2(a_dictionary):
+def multiply_by_2(b_dictionary):
+    a_dictionary = b_dictionary.copy()
     for i in a_dictionary:
         a_dictionary[i] *= 2
     return a_dictionary
