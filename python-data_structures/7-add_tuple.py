7126e8b86d2439d8b8c699df94b025bcbc6fe294
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-12-07 17:08:20 +0400

    gitpusher

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -11,11 +11,11 @@ def add_tuple(tuple_a=(), tuple_b=(), num = 2):
     return tuple(list(map(lambda x, y: x + y, tuple_a, tuple_b))[0:num]) # infinite variables!
 
 def fill_tuple(tuple_test, num = 2):
-    if len(tuple_test) == num:
-        return tuple_test
-    elif len(tuple_test) < num:
+    if len(tuple_test) < num:
         ppa = [0 for i in range(num)]
         for i in range(len(tuple_test)):
             if tuple_test[i] == True:
                 ppa[i] = tuple_test[i]
-    return tuple(ppa)
+        return tuple(ppa)
+    else:
+        return tuple_test[0:num]
