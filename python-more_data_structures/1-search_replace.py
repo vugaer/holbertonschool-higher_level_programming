dcd7b88bf01d86c9f28c7d31d8bd18a525e1919c
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-12-07 19:56:52 +0400

    gitpusher

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,8 +2,8 @@
 
 def search_replace(my_list, search, replace):
     for i in range(len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
+        if my_list[i] == replace:
+            my_list[i] = search
         if my_list[i] == 0:
             my_list[i] = 1
     return my_list
