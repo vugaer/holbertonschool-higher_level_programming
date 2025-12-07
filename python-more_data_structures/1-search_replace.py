f5c665f143c92314117ab949f0d70c553f1b91ae
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-12-07 19:56:30 +0400

    gitpusher

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -5,5 +5,5 @@ def search_replace(my_list, search, replace):
         if my_list[i] == search:
             my_list[i] = replace
         if my_list[i] == 0:
-            my_list[i] == 1
+            my_list[i] = 1
     return my_list
