df3ac5a7ce7b44226013dbb2bdfff2b0a183ddba
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-12-07 20:26:51 +0400

    gitpusher

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
-    newlst = list(set_1+set_2)
-    for i in set_1:
-        if i in set_2:
-            newlst.remove(i)
-    return newlst
+    intersect = [y for y in set_1 if y in set_2]
+    fineshyt = list(set(list(set_1) + list(set_2)))
+    [fineshyt.remove(i) for i in fineshyt if i in intersect]
+    return fineshyt
