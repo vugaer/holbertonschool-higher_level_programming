b5f667e1304adc76a25b9d3166497f00033b5a59
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-12-07 22:00:08 +0400

    gitpusher

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,8 +2,8 @@
 
 def print_sorted_dictionary(a_dictionary):
     newdict = {}
-    keys = sorted(list(a_dictionary.keys()))
-    values = sorted(list(a_dictionary.values()))
+    keys = list(a_dictionary.keys()).sort
+    values = list(a_dictionary.values()).sort
 
     for i in range(len(keys)):
         print("{}: {}".format(keys[i], values[i]))
