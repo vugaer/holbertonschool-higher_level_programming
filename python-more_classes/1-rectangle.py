0f5ade5531b7876fa182f4e3bc28a8abce3ac2b2
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-12-09 06:04:41 +0400

    gitpusher

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""Welcome to Vugar's Tutorial!"""
+
+
+class Rectangle:
+
+    "ts is so tuff"
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    def getWidth(self):
+        return self.__width
+
+    def setWidth(self, value):
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    def getHeight(self):
+        return self.__height
+
+    def setHeight(self, value):
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
