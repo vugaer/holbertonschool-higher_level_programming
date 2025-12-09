33ef8405229aa3ecfd066c18e500ca228adfa0b6
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-12-09 22:41:03 +0400

    gitpusher

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """to run doctest
-
 python3 -m doctest -v tests/1-my_list.txt"""
 
 
 class MyList(list):
     """MyList class that inherits from list"""
-    
+
     def print_sorted(self):
         """Prints the list in sorted ascending order"""
         print(sorted(self))
