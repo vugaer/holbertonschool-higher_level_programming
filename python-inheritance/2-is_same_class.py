e6609159d255f58fe0dee5a515180c314f275f21
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-12-09 22:42:34 +0400

    gitpusher

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,11 +5,9 @@
 def is_same_class(obj, a_class):
     """
     Returns True if obj is exactly an instance of a_class
-    
     Args:
         obj: object to check
         a_class: class to compare against
-        
     Returns:
         True if obj is exactly an instance of a_class, False otherwise
     """
