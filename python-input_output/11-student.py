8412882529822116c45daf750309cf69dc1f87c6
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-12-10 00:47:41 +0400

    gitpusher

---
+++ python-input_output/11-student.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Module that defines a Student class."""
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve a dictionary representation of the Student.
+
+        Args:
+            attrs (list): List of attribute names to retrieve.
+
+        Returns:
+            dict: Dictionary with all or filtered attributes.
+        """
+        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student instance.
+
+        Args:
+            json (dict): Dictionary with attribute names and values.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
