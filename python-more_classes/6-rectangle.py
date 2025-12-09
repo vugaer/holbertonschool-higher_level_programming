1a9c803fb39ece4811f791143c552f21f99dc785
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-12-09 07:10:46 +0400

    gitpusher

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -14,7 +14,7 @@ class Rectangle:
         self.cf(height, "height")
         self.__width = width
         self.__height = height
-        self.number_of_instances += 1
+        Rectangle.number_of_instances += 1
 
     def cf(self, smth, wtf):
         if type(smth) is not int:
@@ -66,4 +66,4 @@ class Rectangle:
 
     def __del__(self):
         print("Bye rectangle...")
-        self.number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
