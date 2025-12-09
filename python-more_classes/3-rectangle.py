5b4156259ac3cc563252c95130c06f2a96b58dc8
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-12-09 06:29:44 +0400

    gitpusher

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -49,7 +49,7 @@ class Rectangle:
     
     def __str__(self):
         if self.__height == 0 or self.__width == 0:
-            return 0
+            return ''
         else:
             for i in range(self.__height):
                 print('#' * self.__width)
