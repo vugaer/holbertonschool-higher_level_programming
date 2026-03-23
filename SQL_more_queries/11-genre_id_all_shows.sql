9e3307371402d84b4f2acd1998594167a8edfae3
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-03-23 23:13:45 +0400

    gitpusher

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- gel ne cekdigimi bir de bana sor
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
