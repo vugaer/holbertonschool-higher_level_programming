e793c14f16a32ac2827ea322144e2a2ff1a1e2ef
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-03-23 17:17:11 +0400

    gitpusher

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- lorem ipsum dolor sit amet
-SELECT score, COUNT(*) AS count FROM second_table GROUP BY score ORDER BY score DESC;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY score DESC;
