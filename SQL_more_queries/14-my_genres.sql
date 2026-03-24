2070e182cbc1f7a48cb8c531f7a7ae314302d65b
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-03-24 13:37:25 +0400

    gitpusher

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -5,4 +5,4 @@ JOIN tv_show_genres ON
 tv_show_genres.genre_id = tv_genres.id
 JOIN tv_shows ON
 tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.name = 'Dexter';
+WHERE tv_shows.title = 'Dexter';
